b751af3399f6b1e86763d3f8baca0cbdbc51aaa6
mgsnowflake <143853774+mgsnowflake@users.noreply.github.com>
2023-09-03 01:17:55 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ streamlit.dataframe(fruityvice_normalized)
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_data_row = my_sur.fetchone()
+my_data_row = my_cur.fetchone()
 streamlist.header("The fruit load list contains:")
 streamlit.dataframe(my_data_row)
 
